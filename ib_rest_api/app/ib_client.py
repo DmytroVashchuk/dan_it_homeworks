c61317cbd3c7d9bacbd789aeabe48adb36fe7ecc
DmytroVashchuk <dimonchik06@gmail.com>
2025-05-13 09:37:31 +0000

    Some change

--- ib_rest_api/app/ib_client.py
+++ ib_rest_api/app/ib_client.py
@@ -1,11 +1,11 @@
 from ib_insync import IB
 import os
 
-def connect_ib():
+async def connect_ib():
     ib = IB()
-    ib_host = os.getenv("IB_HOST", "127.0.0.1")
+    ib_host = os.getenv("IB_HOST", "192.168.112.128")
     ib_port = int(os.getenv("TWS_PORT", "7497"))
     ib_client_id = int(os.getenv("IB_CLIENT_ID", "1"))
 
-    ib.connect(ib_host, ib_port, clientId=ib_client_id)
+    await ib.connectAsync(ib_host, ib_port, clientId=ib_client_id)
     return ib
--- ib_rest_api/app/main.py
+++ ib_rest_api/app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from app.ib_client import connect_ib
+import asyncio
 
 app = FastAPI()
 
 @app.get("/account")
-def get_account_summary():
+async def get_account_summary():
     try:
-        ib = connect_ib()
-        account_summary = ib.accountSummary()
+        ib = await connect_ib()  # чекаємо підключення
+        account_summary = await ib.accountSummaryAsync()  # асинхронний варіант
         result = {item.tag: item.value for item in account_summary}
-        ib.disconnect()
+        await ib.disconnectAsync()
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
